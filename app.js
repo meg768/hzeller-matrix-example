863f4aa4dc9f38e5e6e9bc05f60ff77b50fa8714
meg768 <magnus@egelberg.se>
2016-06-04 20:28:12 +0200

    -

--- app.js
+++ app.js
@@ -8,84 +8,81 @@ var Matrix  = require('hzeller-matrix');
 var matrix  = new Matrix({width:32, height:32});
 
 var App = function() {
-	
+
 	this.run = function() {
 
 		if (args.fill) {
 			var display = matrix.display;
-			
+
 			for (var x = 0; x < matrix.width; x++)
 				for (var y = 0; y < matrix.height; y++)
 					matrix.display.drawPixel(x, y, 0, 0, 255);
-					
+
 			matrix.display.update();
-			
+
 			setTimeout(function(){}, 2000);
 		}
-		
+
 		else if (args.rain) {
 			var options = {};
-			
+
 			extend(options, {duration   : args.duration});
 			extend(options, {delay      : args.delay});
-				
+
 			matrix.runRain(options);
 		}
 
 		else if (args.perlin) {
 			var options = {};
-			
+
 
 			extend(options, {duration   : args.duration});
 			extend(options, {mode       : args.mode});
 			extend(options, {delay      : args.delay});
-				
+
 			matrix.runPerlin(options);
 		}
 
 		else if (args.image) {
 			var options = {};
-			
+
 			extend(options, {scroll     : args.scroll});
 			extend(options, {duration   : args.duration});
 			extend(options, {delay      : args.delay});
 			extend(options, {pause      : args.pause});
-			
+
 			matrix.runImage(args.image, options);
-			
+
 		}
 		else if (args.animation) {
 			var options = {};
-			
+
 			extend(options, {iterations : args.iterations});
 			extend(options, {duration   : args.duration});
 			extend(options, {delay      : args.delay});
-			
+
 			matrix.runAnimation(args.animation, options);
-			
+
 		}
 		else if (args.text) {
 			var options = {};
-			
+
 			extend(options, {textColor : args.textColor});
 			extend(options, {fontName  : args.fontName});
 			extend(options, {fontSize  : args.fontSize});
 			extend(options, {duration  : args.duration});
 			extend(options, {delay     : args.delay});
-			
+
 			matrix.runText(args.text, options);
 		}
 		else {
 			matrix.runText('Hello World!');
-			
+
 		}
 
 	};
 };
 
 
-var app = new App();	
+var app = new App();
 app.run();
-
-
-
